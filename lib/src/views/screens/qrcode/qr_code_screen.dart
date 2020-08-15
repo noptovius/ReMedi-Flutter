6d029dc63b200b460efa0c5a672c65d6cfdaf38a
AntoniKwok <antoniwijaya.kwok@gmail.com>
2020-08-15 18:36:29 +0700

    QR

---
+++ lib/src/views/screens/qrcode/qr_code_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class QrCodeScreen extends StatefulWidget {
+
+  @override
+  _QrCodeScreenState createState() => _QrCodeScreenState();
+}
+
+class _QrCodeScreenState extends State<QrCodeScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+
+}
