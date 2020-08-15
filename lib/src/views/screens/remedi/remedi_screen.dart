e6e847c24706b8740d14dbd6ef05c3c242c4e413
Antoni Wijaya <antoniwijaya.kwok@gmail.com>
2020-08-15 08:23:31 +0000

    Feature/remedi screen

---
+++ lib/src/views/screens/remedi/remedi_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:garudahacks/src/utils/extensions.dart';
+import 'package:garudahacks/src/views/screens/remedi/remedi_content.dart';
+import 'package:garudahacks/src/views/widgets/common/app_theme.dart';
+import 'package:garudahacks/src/views/widgets/common/custom_title_text.dart';
+
+class ReMediScreen extends StatefulWidget {
+
+  @override
+  _ReMediScreenState createState() => _ReMediScreenState();
+}
+
+class _ReMediScreenState extends State<ReMediScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Container(
+                height: AppTheme.fullHeight(context) - 50,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xfffbfbfb),
+                      Color(0xfff7f7f7),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _appBar(),
+                    _title(),
+                    Expanded(
+                      child: AnimatedSwitcher(
+                        duration: Duration(milliseconds: 300),
+                        switchInCurve: Curves.easeInToLinear,
+                        switchOutCurve: Curves.easeOutBack,
+                        child: ReMediContent()
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _appBar() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          _icon(
+            Icons.arrow_back_ios,
+            color: Colors.black54,
+            size: 15,
+            padding: 12,
+            isOutLine: true,
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TitleText(text: "Medical Record Details", fontSize: 14, fontWeight: FontWeight.w200,)
+        ],
+      ),
+    );
+  }
+
+  Widget _title() {
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TitleText(
+                  text: "Myocard Infarct",
+                  fontSize: 27,
+                  fontWeight: FontWeight.w400,
+                ),
+                TitleText(
+                  text: "Ongoing Treatment",
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                ),
+              ],
+            ),
+            Spacer(),
+          ],
+        ));
+  }
+
+  Widget _icon(
+      IconData icon, {
+        Color color = Colors.white,
+        double size = 20,
+        double padding = 10,
+        bool isOutLine = false,
+        Function onPressed,
+      }) {
+    return Container(
+      height: 40,
+      width: 40,
+      padding: EdgeInsets.all(padding),
+      // margin: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        border: Border.all(
+            color: Colors.white,
+            style: isOutLine ? BorderStyle.solid : BorderStyle.none),
+        borderRadius: BorderRadius.all(Radius.circular(13)),
+        color:
+        isOutLine ? Colors.transparent : Theme.of(context).backgroundColor,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Color(0xfff8f8f8),
+              blurRadius: 5,
+              spreadRadius: 10,
+              offset: Offset(5, 5)),
+        ],
+      ),
+      child: Icon(icon, color: color, size: size),
+    ).ripple(() {
+      if (onPressed != null) {
+        onPressed();
+      }
+    }, borderRadius: BorderRadius.all(Radius.circular(13)));
+  }
+
+}
